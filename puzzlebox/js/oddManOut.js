d1c10765da708fda8817df1740168659aa96ae93
hariraj <hariraj.vadivelu@gmail.com>
2015-03-08 11:48:32 +0530

    changed odd man out file fetch logic

--- puzzlebox/js/oddManOut.js
+++ puzzlebox/js/oddManOut.js
@@ -14,35 +14,32 @@ $(document).ready(function () {
 		getImages: function () {
 			$(".quizy-mg-notification-fly").hide();
 			var imgDir = "images/oddman/";
-			var imagesFolderArray = ["chairs/", "animals/", "games/", "phones/", "obama/"];
+			var imagesFolderArray = ["chair", "animal", "game", "phone", "obama"];
 			var dirIndex = this.getRandomArbitrary(imagesFolderArray.length);
-			var dir = imgDir + imagesFolderArray[dirIndex];
+			var category = imagesFolderArray[dirIndex];
+			var dir = imgDir + category +"/";
 			var listOfImages = "";
 			var listImgArr = [];
-			var fileExt = [".gif", ".png", ".jpg"];
 
-			$.ajax({
-				url: dir,
-				success: function (data) {
-					$(data).find("a").each(function () {
-						var file = $(this).attr("href");
-						$.each(fileExt, function (key, val) {
-							if(file.indexOf(val) > -1) {
-							    listImgArr.push("<li><img class='oddManOut ui-btn' data-about='" + file + "' src='" + dir + file + "' /></li>");
-							}
-						});
-					});
-
-					listImgArr.sort(function () {
-						return 0.5 - Math.random();
-					});
-
-					for(var i = 0; i < listImgArr.length; i++) {
-						listOfImages += listImgArr[i];
-					}
-					$("#imagesSection").html("<ul class='oddManOutSection'>" + listOfImages + "</ul>");
+			var selection = ["1.png","2.png","3.png","4.png","odd.png"];
+			for(var i = 0; i < 5; i++) {
+				var randomPosition = Math.floor(Math.random() * selection.length);
+				var selected = selection.splice(randomPosition,1);
+				var imageName = category + selected;
+				if(selected == "odd.png"){
+					imageName = selected;
 				}
+				listImgArr.push("<li><img class='oddManOut ui-btn' data-about='" + imageName + "' src='" + dir + imageName + "' /></li>");
+			}
+			listImgArr.sort(function () {
+				return 0.5 - Math.random();
 			});
+
+			for(var i = 0; i < listImgArr.length; i++) {
+				listOfImages += listImgArr[i];
+			}
+			$("#imagesSection").html("<ul class='oddManOutSection'>" + listOfImages + "</ul>");
+			
 		},
 
         //shows and hids the correct/wrong message after an action
